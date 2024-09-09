56330c5ad223a6fb75327f32e016d620c8886934
Nikola Kenjic <nikola.nico89@gmail.com>
2024-09-09 20:22:18 +0200

    controllers and model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -88,18 +88,37 @@ exports.getSingleTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+
+    res.status(200).json({
     status: 'success',
     data: {
-      tour: 'Update Tour',
+      tour
     },
   });
+  } catch (err) {
+     res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  } 
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(200).json({
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
+
+    res.status(204).json({
     status: 'success',
     data: null,
   });
+  } catch (err) {
+     res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
+ 
 };
--- models/tourModel.js
+++ models/tourModel.js
@@ -4,15 +4,37 @@ const TourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a price'],
-        unique: true
+        unique: true,
+          trim: true
     },
-    rating: {
+    durations: {
+        type:Number,
+        required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price:{
         type: Number,
         required: [true, 'A tour must have a price']
+    },
+    priceDiscount: Number,
+    summery: {
+        type: String,
+        trim: true
     }
 })
 
