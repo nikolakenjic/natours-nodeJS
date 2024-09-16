5c3ea9ec252adf7d9a8f0073b0e47abdac659a92
Nikola Kenjic <nikola.nico89@gmail.com>
2024-09-16 15:44:22 +0200

    doc middleware

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from middleware');
+//   next();
+// });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 // Routes
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,56 +1,73 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const TourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a price'],
-        unique: true,
-        trim: true
-    },
-    duration: {
-        type:Number,
-        required: [true, 'A tour must have a duration'],
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size'],
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty'],
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4.5
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0
-    },
-    price:{
-        type: Number,
-        required: [true, 'A tour must have a price']
-    },
-    priceDiscount: Number,
-    summary: {
-        type: String,
-        trim: true
-    },
-    description: {
-        type: String,
-        trim: true
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image']
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
-    startDates: [Date]
-})
+  name: {
+    type: String,
+    required: [true, 'A tour must have a price'],
+    unique: true,
+    trim: true,
+  },
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
+});
 
-module.exports = mongoose.model('Tour', TourSchema)
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+TourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
+// TourSchema.pre('save', function (next) {
+//   console.log('Document saving...');
+//   next();
+// });
+
+// TourSchema.post('save', function (doc, next) {
+//   console.log('Doc', doc);
+//   next();
+// });
+
+module.exports = mongoose.model('Tour', TourSchema);
