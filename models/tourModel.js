5be117e8667804c1018bbb6802c30ae6b7f33bdc
Nikola Kenjic <nikola.nico89@gmail.com>
2024-09-18 20:13:40 +0200

    user model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+// const validator = require('validator');
 
 const TourSchema = new mongoose.Schema({
   name: {
