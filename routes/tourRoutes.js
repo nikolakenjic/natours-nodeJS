7f7be4c5948a778b0083684c51522989fe679003
Nikola Kenjic <nikola.nico89@gmail.com>
2024-09-23 19:41:26 +0200

    permission

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,7 +11,7 @@ const {
   // checkID,
   // checkBody,
 } = require('./../controllers/tourController');
-const { protect } = require('../controllers/authController');
+const { protect, restrictedTo } = require('../controllers/authController');
 
 // router.param('id', checkID);
 
@@ -21,6 +21,10 @@ router.route('/top-5-cheap').get(top5Cheap, getAllTours);
 router.route('/monthly-plan/:year').get(top5Cheap, getMonthlyPlan);
 
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getSingleTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/:id')
+  .get(getSingleTour)
+  .patch(updateTour)
+  .delete(protect, restrictedTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
