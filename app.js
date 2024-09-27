2150fa55e0a9d16c4a6a024bc865522a8c1e48af
Nikola Kenjic <nikola.nico89@gmail.com>
2024-09-27 19:29:17 +0200

    security

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalError = require('./controllers/errorController');
@@ -8,19 +13,49 @@ const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
 
+// GLOBAL MIDDLEWARES
+// Set Security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// MIDDLEWARE
-app.use(express.json());
+// Limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in one hour!',
+});
+
+app.use('/api', limiter);
+
+// Body parser, reading data from body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution from query
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
-
-// app.use((req, res, next) => {
-//   console.log(req.headers);
-
-//   next();
-// });
 
 // Routes
 app.use('/api/v1/users', userRouter);
