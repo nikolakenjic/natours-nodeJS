99540c591ca6cb77aade3e51062c48a35af4cae7
Nikola Kenjic <nikola.nico89@gmail.com>
2024-09-28 15:01:04 +0200

    review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -4,22 +4,27 @@ const mongoose = require('mongoose');
 const ReviewSchema = new mongoose.Schema({
   review: {
     type: String,
+    required: [true, 'Review can not be empty'],
   },
   rating: {
     type: Number,
+    min: 1,
+    max: 5,
     default: 4.5,
   },
   createdAt: {
     type: Date,
     default: Date.now(),
   },
-  tourId: {
+  tour: {
     type: mongoose.Schema.ObjectId,
     ref: 'Tour',
+    required: [true, 'Review must belong to a tour.'],
   },
-  userId: {
-    ype: mongoose.Schema.ObjectId,
+  user: {
+    type: mongoose.Schema.ObjectId,
     ref: 'User',
+    required: [true, 'Review must belong to a user.'],
   },
 });
 
