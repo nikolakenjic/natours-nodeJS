86c2ecb94d475c609e8182278aecca635a95e844
Nikola Kenjic <nikola.nico89@gmail.com>
2024-10-07 16:31:12 +0200

    pug

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -13,8 +13,28 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  // 1.Get  data for requested Tour (including reviews and guides)
+  // const slugToName = (slug) => {
+  //   return slug
+  //     .split('-') // Split by hyphen (-)
+  //     .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter of each word
+  //     .join(' '); // Join words with spaces
+  // };
+
+  // const tourName = slugToName(req.params.tourName);
+  // const tour = await Tour.findOne({ name: tourName });
+
+  const tour = await Tour.findOne({ slug: req.params.tourName }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  console.log('Tour', tour);
+
+  // 2.Build template
+
+  // 3.Render that template using data from 1.
+  res.status(200).render('tour', {
+    tour,
+  });
+});
